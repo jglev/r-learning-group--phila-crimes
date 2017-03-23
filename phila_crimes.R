a76e62e3a33e42ae067c84080ea4f7de709e0e6f
Jacob Levernier <jlevern@upenn.edu>
2017-03-23 11:09:46 -0400

    Applied changes from @stevegbrooks for automating download of dataset.

--- phila_crimes.R
+++ phila_crimes.R
@@ -1,3 +1,17 @@
 ##DOWNLOAD CRIME DATA##
-download.file("https://data.phila.gov/api/views/sspu-uyfa/rows.csv", destfile ="phila_crimes_scraped",method ="curl")
-phila_crimes<-read.csv("~/Downloads/phila_crimes_scraped")
+
+library(data.table)
+
+library(RCurl)
+
+library(curl)
+
+
+
+url <- "https://data.phila.gov/api/views/sspu-uyfa/rows.csv"
+
+
+
+phila_crimes<-fread(url)
+
+
