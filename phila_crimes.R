8dcaacd2157c76979b7c5c64cd764fc43770ec81
Steven Brooks <stevegbrooks@gmail.com>
2017-03-30 10:11:39 -0400

    Instead of downloading file from OpenDataPhilly, we now have a dataset on GitHub which is a small random sample of the original.

--- phila_crimes.R
+++ phila_crimes.R
@@ -1,18 +1,21 @@
-##INSTALL PACKAGES##
+#INSTALL/LOAD NECESSARY PACKAGES#
 
 if(!require(data.table) |
    !require(RCurl) |
-   !require(curl)) {
-  install.packages(c('data.table','RCurl','curl'))
+   !require(curl) |
+   !require(dplyr) |
+   !require(readr)) {
+  install.packages(
+    c('data.table','RCurl','curl','dplyr','readr'))
 }
 
 library(data.table)
 library(RCurl)
 library(curl)
+library(dplyr)
+library(readr)
 
-##DOWNLOAD CRIME DATA##
+##IMPORT DATA##
 
-url <- "https://data.phila.gov/api/views/sspu-uyfa/rows.csv"
-phila_crimes<-fread(url)
-
-
+phila_crimes_10percent <- read_csv("#YOUR WD#/r-learning-group--phila-crimes/phila_crimes_10percent.csv")
+View(phila_crimes_10percent)
