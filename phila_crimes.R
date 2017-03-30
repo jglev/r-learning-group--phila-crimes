d788f5e80e7b834cc5e3241e6a359043c974348c
Steven Brooks <stevegbrooks@gmail.com>
2017-03-30 09:19:06 -0400

    code for checking whether packages are installed, and installing them if they are not.

--- phila_crimes.R
+++ phila_crimes.R
@@ -1,17 +1,18 @@
-##DOWNLOAD CRIME DATA##
+##INSTALL PACKAGES##
+
+if(!require(data.table) |
+   !require(RCurl) |
+   !require(curl)) {
+  install.packages(c('data.table','RCurl','curl'))
+}
 
 library(data.table)
-
 library(RCurl)
-
 library(curl)
 
-
+##DOWNLOAD CRIME DATA##
 
 url <- "https://data.phila.gov/api/views/sspu-uyfa/rows.csv"
-
-
-
 phila_crimes<-fread(url)
 
 
